8d8c9bc0dd3113ae51b55b12c195b6f6b55dcd57
merakm <kumazaki98+github@gmail.com>
2018-10-21 15:03:16 +0900

    implement transfer function

--- contracts/SprinterOwnership.sol
+++ contracts/SprinterOwnership.sol
@@ -14,4 +14,20 @@ contract SprinterOwnership is SprinterCompetition, ERC721Basic {
         return sprinterOwner[_tokenId];
     }
 
+
+    function _transfer( address _from, address _to, uint256 _tokenId) private {
+
+        require(ownerOf(_tokenId) == _from, "this sprinter is not owned by from address");
+
+        sprinterOwner[_tokenId] = _to;
+        ownerSprinterCount[_from] = ownerSprinterCount[_from].sub(1);
+        ownerSprinterCount[_to] = ownerSprinterCount[_to].add(1);
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+        _transfer(msg.sender, _to, _tokenId);
+    }
+
 }
